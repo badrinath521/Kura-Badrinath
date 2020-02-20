fbd004400df9109500c3d15d7f95571515f87cf7
badrinath521 <kurabadrinath@gmai.com>
2020-02-20 09:11:34 +0530

    task4

--- maven-demo/src/main/java/com/badri/mavendemo/Calculatearea.java
+++ maven-demo/src/main/java/com/badri/mavendemo/Calculatearea.java
@@ -1,16 +1,10 @@
 package com.badri.mavendemo;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.util.Scanner;
 
 public class Calculatearea {
 	 public static void main( String[] args ) throws IOException
 	 {
-			Scanner sc = new Scanner(System.in);
-	    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	    	bw.write(" please select option"+"\n");
-	    	bw.flush();
+			
 	    	HouseConstruction a=new HouseConstruction();
 	    	a.valuesofmaterials();
 	 }
